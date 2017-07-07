4e5fd3b55e7548fcff2a3b6d362d0b77c0093411
villedevs2 <villedevs@gmail.com>
2017-07-08 02:32:59 +0300

    fix preview

--- src/Preview.cpp
+++ src/Preview.cpp
@@ -5,8 +5,8 @@
 
 GLPreview::GLPreview(QWidget* parent, Level* level) : QGLWidget(QGLFormat(QGL::SampleBuffers), parent)
 {
-	m_scroll = glm::vec2(0.0f, 0.0f);
-	m_scroll_saved = glm::vec2(0.0f, 0.0f);
+	m_scroll = glm::vec2(16.0f, 16.0f);
+	m_scroll_saved = m_scroll;
 }
 
 GLPreview::~GLPreview()
@@ -40,8 +40,8 @@ void GLPreview::loadTexture(QImage* texture)
 	glGenTextures(1, &m_base_tex);
 	glBindTexture(GL_TEXTURE_2D, m_base_tex);
 
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
@@ -108,45 +108,47 @@ void GLPreview::initializeGL()
 	m_level_shader.scale = m_level_program->uniformLocation("v_scale");
 	m_level_shader.vp_matrix = m_level_program->uniformLocation("m_vp_matrix");
 
-	m_vbo[0].pos = glm::vec3(0.0f, 0.0f, 0.0f);		m_vbo[0].uv = glm::vec2(0.0f, 0.0f);		m_vbo[0].color = 0xffffffff;
-	m_vbo[1].pos = glm::vec3(0.0f, 32.0f, 0.0f);	m_vbo[1].uv = glm::vec2(0.0f, 1.0f);		m_vbo[1].color = 0xffffffff;
-	m_vbo[2].pos = glm::vec3(32.0f, 32.0f, 0.0f);	m_vbo[2].uv = glm::vec2(1.0f, 1.0f);		m_vbo[2].color = 0xffffffff;
+	float zh = 2.0f;
 
-	m_vbo[3].pos = glm::vec3(0.0f, 0.0f, 0.0f);		m_vbo[3].uv = glm::vec2(0.0f, 0.0f);		m_vbo[3].color = 0xffffffff;
-	m_vbo[4].pos = glm::vec3(32.0f, 0.0f, 0.0f);	m_vbo[4].uv = glm::vec2(1.0f, 0.0f);		m_vbo[4].color = 0xffffffff;
-	m_vbo[5].pos = glm::vec3(32.0f, 32.0f, 0.0f);	m_vbo[5].uv = glm::vec2(1.0f, 1.0f);		m_vbo[5].color = 0xffffffff;
+	m_vbo[0].pos = glm::vec3(0.0f, 0.0f, 0.0f);		m_vbo[0].uv = glm::vec2(0.0f, 0.0f);		m_vbo[0].color = 0xff808080;
+	m_vbo[1].pos = glm::vec3(0.0f, 32.0f, 0.0f);	m_vbo[1].uv = glm::vec2(0.0f, 1.0f);		m_vbo[1].color = 0xff808080;
+	m_vbo[2].pos = glm::vec3(32.0f, 32.0f, 0.0f);	m_vbo[2].uv = glm::vec2(1.0f, 1.0f);		m_vbo[2].color = 0xff808080;
 
-	// --
-	m_vbo[6].pos = glm::vec3(4.0f, 5.0f, 0.0f);		m_vbo[6].uv = glm::vec2(0.0f, 1.0f);		m_vbo[6].color = 0xffffffff;
-	m_vbo[7].pos = glm::vec3(4.0f, 4.0f, 0.0f);		m_vbo[7].uv = glm::vec2(1.0f, 1.0f);		m_vbo[7].color = 0xffffffff;
-	m_vbo[8].pos = glm::vec3(4.0f, 4.0f, 10.0f);	m_vbo[8].uv = glm::vec2(1.0f, 0.0f);		m_vbo[8].color = 0xffffffff;
-	m_vbo[9].pos = glm::vec3(4.0f, 5.0f, 0.0f);		m_vbo[9].uv = glm::vec2(0.0f, 1.0f);		m_vbo[9].color = 0xffffffff;
-	m_vbo[10].pos = glm::vec3(4.0f, 5.0f, 10.0f);	m_vbo[10].uv = glm::vec2(0.0f, 0.0f);		m_vbo[10].color = 0xffffffff;
-	m_vbo[11].pos = glm::vec3(4.0f, 4.0f, 10.0f);	m_vbo[11].uv = glm::vec2(1.0f, 0.0f);		m_vbo[11].color = 0xffffffff;
+	m_vbo[3].pos = glm::vec3(0.0f, 0.0f, 0.0f);		m_vbo[3].uv = glm::vec2(0.0f, 0.0f);		m_vbo[3].color = 0xff808080;
+	m_vbo[4].pos = glm::vec3(32.0f, 32.0f, 0.0f);	m_vbo[4].uv = glm::vec2(1.0f, 1.0f);		m_vbo[4].color = 0xff808080;
+	m_vbo[5].pos = glm::vec3(32.0f, 0.0f, 0.0f);	m_vbo[5].uv = glm::vec2(1.0f, 0.0f);		m_vbo[5].color = 0xff808080;
 
 	// --
-	m_vbo[12].pos = glm::vec3(3.0f, 5.0f, 0.0f);	m_vbo[12].uv = glm::vec2(0.0f, 1.0f);		m_vbo[12].color = 0xffffffff;
-	m_vbo[13].pos = glm::vec3(4.0f, 5.0f, 0.0f);	m_vbo[13].uv = glm::vec2(1.0f, 1.0f);		m_vbo[13].color = 0xffffffff;
-	m_vbo[14].pos = glm::vec3(4.0f, 5.0f, 10.0f);	m_vbo[14].uv = glm::vec2(1.0f, 0.0f);		m_vbo[14].color = 0xffffffff;
-	m_vbo[15].pos = glm::vec3(3.0f, 5.0f, 0.0f);	m_vbo[15].uv = glm::vec2(0.0f, 1.0f);		m_vbo[15].color = 0xffffffff;
-	m_vbo[16].pos = glm::vec3(3.0f, 5.0f, 10.0f);	m_vbo[16].uv = glm::vec2(0.0f, 0.0f);		m_vbo[16].color = 0xffffffff;
-	m_vbo[17].pos = glm::vec3(4.0f, 5.0f, 10.0f);	m_vbo[17].uv = glm::vec2(1.0f, 0.0f);		m_vbo[17].color = 0xffffffff;
+	m_vbo[6].pos = glm::vec3(14.0f, 15.0f, 0.0f);		m_vbo[6].uv = glm::vec2(0.0f, 1.0f);		m_vbo[6].color = 0xff303030;
+	m_vbo[7].pos = glm::vec3(14.0f, 14.0f, 0.0f);		m_vbo[7].uv = glm::vec2(1.0f, 1.0f);		m_vbo[7].color = 0xff303030;
+	m_vbo[8].pos = glm::vec3(14.0f, 14.0f, zh);			m_vbo[8].uv = glm::vec2(1.0f, 0.0f);		m_vbo[8].color = 0xff303030;
+	m_vbo[9].pos = glm::vec3(14.0f, 15.0f, 0.0f);		m_vbo[9].uv = glm::vec2(0.0f, 1.0f);		m_vbo[9].color = 0xff303030;
+	m_vbo[11].pos = glm::vec3(14.0f, 15.0f, zh);		m_vbo[11].uv = glm::vec2(0.0f, 0.0f);		m_vbo[11].color = 0xff303030;
+	m_vbo[10].pos = glm::vec3(14.0f, 14.0f, zh);		m_vbo[10].uv = glm::vec2(1.0f, 0.0f);		m_vbo[10].color = 0xff303030;
 
 	// --
-	m_vbo[18].pos = glm::vec3(3.0f, 4.0f, 0.0f);	m_vbo[18].uv = glm::vec2(0.0f, 1.0f);		m_vbo[18].color = 0xffffffff;
-	m_vbo[19].pos = glm::vec3(4.0f, 4.0f, 10.0f);	m_vbo[19].uv = glm::vec2(1.0f, 0.0f);		m_vbo[19].color = 0xffffffff;
-	m_vbo[20].pos = glm::vec3(4.0f, 4.0f, 0.0f);	m_vbo[20].uv = glm::vec2(1.0f, 1.0f);		m_vbo[20].color = 0xffffffff;
-	m_vbo[21].pos = glm::vec3(3.0f, 4.0f, 0.0f);	m_vbo[21].uv = glm::vec2(0.0f, 1.0f);		m_vbo[21].color = 0xffffffff;
-	m_vbo[22].pos = glm::vec3(3.0f, 4.0f, 10.0f);	m_vbo[22].uv = glm::vec2(0.0f, 0.0f);		m_vbo[22].color = 0xffffffff;
-	m_vbo[23].pos = glm::vec3(4.0f, 4.0f, 10.0f);	m_vbo[23].uv = glm::vec2(1.0f, 0.0f);		m_vbo[23].color = 0xffffffff;
+	m_vbo[12].pos = glm::vec3(13.0f, 15.0f, 0.0f);		m_vbo[12].uv = glm::vec2(0.0f, 1.0f);		m_vbo[12].color = 0xff101010;
+	m_vbo[13].pos = glm::vec3(14.0f, 15.0f, 0.0f);		m_vbo[13].uv = glm::vec2(1.0f, 1.0f);		m_vbo[13].color = 0xff101010;
+	m_vbo[14].pos = glm::vec3(14.0f, 15.0f, zh);		m_vbo[14].uv = glm::vec2(1.0f, 0.0f);		m_vbo[14].color = 0xff101010;
+	m_vbo[15].pos = glm::vec3(13.0f, 15.0f, 0.0f);		m_vbo[15].uv = glm::vec2(0.0f, 1.0f);		m_vbo[15].color = 0xff101010;
+	m_vbo[17].pos = glm::vec3(13.0f, 15.0f, zh);		m_vbo[17].uv = glm::vec2(0.0f, 0.0f);		m_vbo[17].color = 0xff101010;
+	m_vbo[16].pos = glm::vec3(14.0f, 15.0f, zh);		m_vbo[16].uv = glm::vec2(1.0f, 0.0f);		m_vbo[16].color = 0xff101010;
 
 	// --
-	m_vbo[24].pos = glm::vec3(3.0f, 4.0f, 10.0f);	m_vbo[24].uv = glm::vec2(0.0f, 0.0f);		m_vbo[24].color = 0xffffffff;
-	m_vbo[25].pos = glm::vec3(4.0f, 4.0f, 10.0f);	m_vbo[25].uv = glm::vec2(1.0f, 0.0f);		m_vbo[25].color = 0xffffffff;
-	m_vbo[26].pos = glm::vec3(4.0f, 5.0f, 10.0f);	m_vbo[26].uv = glm::vec2(1.0f, 1.0f);		m_vbo[26].color = 0xffffffff;
-	m_vbo[27].pos = glm::vec3(3.0f, 4.0f, 10.0f);	m_vbo[27].uv = glm::vec2(0.0f, 0.0f);		m_vbo[27].color = 0xffffffff;
-	m_vbo[28].pos = glm::vec3(3.0f, 5.0f, 10.0f);	m_vbo[28].uv = glm::vec2(0.0f, 1.0f);		m_vbo[28].color = 0xffffffff;
-	m_vbo[29].pos = glm::vec3(4.0f, 5.0f, 10.0f);	m_vbo[29].uv = glm::vec2(1.0f, 1.0f);		m_vbo[29].color = 0xffffffff;
+	m_vbo[18].pos = glm::vec3(13.0f, 14.0f, 0.0f);		m_vbo[18].uv = glm::vec2(0.0f, 1.0f);		m_vbo[18].color = 0xffffffff;
+	m_vbo[19].pos = glm::vec3(14.0f, 14.0f, zh);		m_vbo[19].uv = glm::vec2(1.0f, 0.0f);		m_vbo[19].color = 0xffffffff;
+	m_vbo[20].pos = glm::vec3(14.0f, 14.0f, 0.0f);		m_vbo[20].uv = glm::vec2(1.0f, 1.0f);		m_vbo[20].color = 0xffffffff;
+	m_vbo[21].pos = glm::vec3(13.0f, 14.0f, 0.0f);		m_vbo[21].uv = glm::vec2(0.0f, 1.0f);		m_vbo[21].color = 0xffffffff;
+	m_vbo[22].pos = glm::vec3(13.0f, 14.0f, zh);		m_vbo[22].uv = glm::vec2(0.0f, 0.0f);		m_vbo[22].color = 0xffffffff;
+	m_vbo[23].pos = glm::vec3(14.0f, 14.0f, zh);		m_vbo[23].uv = glm::vec2(1.0f, 0.0f);		m_vbo[23].color = 0xffffffff;
+
+	// --
+	m_vbo[24].pos = glm::vec3(13.0f, 14.0f, zh);		m_vbo[24].uv = glm::vec2(0.0f, 0.0f);		m_vbo[24].color = 0xff808080;
+	m_vbo[26].pos = glm::vec3(14.0f, 14.0f, zh);		m_vbo[26].uv = glm::vec2(1.0f, 0.0f);		m_vbo[26].color = 0xff808080;
+	m_vbo[25].pos = glm::vec3(14.0f, 15.0f, zh);		m_vbo[25].uv = glm::vec2(1.0f, 1.0f);		m_vbo[25].color = 0xff808080;
+	m_vbo[27].pos = glm::vec3(13.0f, 14.0f, zh);		m_vbo[27].uv = glm::vec2(0.0f, 0.0f);		m_vbo[27].color = 0xff808080;
+	m_vbo[28].pos = glm::vec3(13.0f, 15.0f, zh);		m_vbo[28].uv = glm::vec2(0.0f, 1.0f);		m_vbo[28].color = 0xff808080;
+	m_vbo[29].pos = glm::vec3(14.0f, 15.0f, zh);		m_vbo[29].uv = glm::vec2(1.0f, 1.0f);		m_vbo[29].color = 0xff808080;
 }
 
 void GLPreview::paintGL()
@@ -162,8 +164,8 @@ void GLPreview::paintGL()
 
 	// opengl scene rendering
 	// --------------------------------------------------------------------------
-	glDisable(GL_CULL_FACE);
-	glDisable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+	glEnable(GL_DEPTH_TEST);
 
 	qglClearColor(QColor(0,64,192));
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -176,14 +178,17 @@ void GLPreview::paintGL()
 	float halfw = (float)((LEVEL_VIS_WIDTH) / 2);
 	float halfh = halfw / aspect;
 
-	float size = 0.01f * (float)tan((100.0 * M_PI / 180.0) / 2);
+	double fov = 60.0;
+
+	float size = 0.01f * (float)tan((fov * M_PI / 180.0) / 2);
+
+	float camera_distance = ((float)(LEVEL_VIS_WIDTH) / 2) / tan((fov * M_PI / 180.0) / 2);
 
 
-	glm::vec3 pos = glm::vec3(20.0f, 20.0f, 60.0f);
-	glm::vec3 eye = glm::vec3(0.0f, 40.0f, 0.0f);
+	glm::vec3 pos = glm::vec3(m_scroll.x, m_scroll.y+0.0f, camera_distance);
+	glm::vec3 eye = glm::vec3(m_scroll.x, m_scroll.y, 0.0f);
 
-	//glm::mat4 camera_proj_matrix = glm::frustum<float>(size, size, size / aspect, size / aspect, 0.01f, 100.0f);
-	glm::mat4 camera_proj_matrix = glm::perspective(glm::radians(45.0f), aspect, 0.1f, 100.0f);
+	glm::mat4 camera_proj_matrix = glm::frustum<float>(-size, size, size / aspect, -size / aspect, 0.01f, 100.0f);
 	glm::mat4 camera_view_matrix = glm::lookAt(pos, eye, glm::vec3(0.0f, 1.0f, 0.0));
 	glm::mat4 camera_vp_matrix = camera_proj_matrix * camera_view_matrix;
 
@@ -202,7 +207,7 @@ void GLPreview::paintGL()
 	m_level_program->bind();
 
 	m_level_program->setUniformValue(m_level_shader.vp_matrix, vp_mat);
-	m_level_program->setUniformValue(m_level_shader.location, m_scroll.x, m_scroll.y);
+	//m_level_program->setUniformValue(m_level_shader.location, m_scroll.x, m_scroll.y);
 
 	m_level_program->enableAttributeArray(m_level_shader.position);
 	m_level_program->setAttributeArray(m_level_shader.position, (GLfloat*)m_vbo, 3, sizeof(VBO));
